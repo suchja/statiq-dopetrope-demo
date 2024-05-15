308513dabb011711a3c249c5097c85e7af55158d
Jan (LernMoment.de) <jan@lernmoment.de>
2024-05-15 18:59:39 +0200

    Setup initial deployment to GH-Pages

--- statiq-dopetrope-demo/Program.cs
+++ statiq-dopetrope-demo/Program.cs
@@ -1,5 +1,10 @@
 ﻿return await Bootstrapper
   .Factory
   .CreateWeb(args)
+  .DeployToGitHubPagesBranch(
+        "suchja",
+        "suchja.github.io",
+        Config.FromSetting<string>("GITHUB_TOKEN"),
+        "main")
   .RunAsync();
   
