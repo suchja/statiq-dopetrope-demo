6dcdbf0e9b12d286596f7265c5602a76b10c36d2
Jan (LernMoment.de) <jan@lernmoment.de>
2024-05-22 10:18:11 +0200

    Let GH-Action deploy instead of statiq pipeline

--- statiq-dopetrope-demo/Program.cs
+++ statiq-dopetrope-demo/Program.cs
@@ -1,10 +1,5 @@
 ﻿return await Bootstrapper
   .Factory
   .CreateWeb(args)
-  .DeployToGitHubPagesBranch(
-        "suchja",
-        "statiq-dopetrope-demo",
-        Config.FromSetting<string>("GITHUB_TOKEN"),
-        "gh-pages")
   .RunAsync();
   
